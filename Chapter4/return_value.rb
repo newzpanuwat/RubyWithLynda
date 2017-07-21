514e0a0e68b8ea9884a22856ef1414d11d632463
newzpanuwat <new_sbh@me.com>
2017-07-21 11:09:33 +0700

    add return_value lesson

--- Chapter4/return_value.rb
+++ Chapter4/return_value.rb
@@ -1,28 +1,42 @@
 
 def welcome(name="World")
-	puts "Hello #{name}!"
+   puts "1.)Hello #{name}!"
+   2+2
 end
 
-def add(n1=0, n2=0)
-	puts n1 + n2
+def add_and_substract(n1=0, n2=0)
+  add = n1 + n2
+  sub = n1 - n2
+  return [add, sub]
 end
 
 def longest_word(words=[])
   longest_word = words.inject do |memo,word|
     memo.length > word.length ? memo : word
   end
-  puts longest_word
+  return longest_word
 end
 
 def over_five?(value=nil)
-	puts value.to_i > 5 ? 'Over 5' : 'Not over 5'
+  return "Exactly 5" if value.to_i == 5
+  if value.to_i > 5
+    return "3.)Over 5,that input data is #{value.to_i}"
+  else
+    return "3.)Under 5,that input data is #{value.to_i}"
+  end
 end
+
+
+#/////
 
 welcome("Mary")
 
-add(2, 2)
+result = add_and_substract(2, 2)
+puts result[0]
+puts result[1]
+
 
 fruits = ['apple', 'pear', 'banana', 'plum']
-longest_word(fruits)
+puts longest_word(fruits)
 
-over_five?(112 / 18)
+puts over_five?(5 / 5)
